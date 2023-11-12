56a6a756ee64cdb435d80a3edc7c0221761ee1dd
pierpaolo1989 <pierpaolo.pdd@gmail.com>
2023-11-12 16:59:21 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Site made with Mobirise Website Builder v5.2.0, https://mobirise.com -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="generator" content="Mobirise v5.2.0, mobirise.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+    <link rel="icon" type="image/svg+xml" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 width=%22256%22 height=%22256%22 viewBox=%220 0 100 100%22><rect width=%22100%22 height=%22100%22 rx=%2220%22 fill=%22%23ffffff%22></rect><path fill=%22%23bd10e0%22 d=%22M18.22 54.78L12.77 54.78L12.77 66.39Q12.44 66.50 11.90 66.63Q11.36 66.77 10.72 66.77L10.72 66.77Q8.34 66.77 8.34 64.77L8.34 64.77L8.34 36.04Q8.34 35.18 8.77 34.72Q9.20 34.26 10.12 33.99L10.12 33.99Q11.69 33.50 13.93 33.29Q16.17 33.07 18.28 33.07L18.28 33.07Q25.13 33.07 28.29 35.96Q31.45 38.85 31.45 43.76L31.45 43.76Q31.45 48.78 28.21 51.78Q24.97 54.78 18.22 54.78L18.22 54.78ZM12.71 51.11L17.95 51.11Q22.27 51.11 24.65 49.33Q27.02 47.54 27.02 43.82L27.02 43.82Q27.02 40.14 24.73 38.44Q22.43 36.74 18.17 36.74L18.17 36.74Q16.71 36.74 15.28 36.88Q13.85 37.01 12.71 37.23L12.71 37.23L12.71 51.11ZM35.88 54.45L51.59 33.61Q52.08 33.50 52.70 33.42Q53.32 33.34 53.81 33.34L53.81 33.34Q54.94 33.34 55.86 33.88Q56.78 34.42 56.78 35.99L56.78 35.99L56.78 54.89L62.18 54.89Q62.34 55.16 62.50 55.62Q62.66 56.08 62.66 56.67L62.66 56.67Q62.66 58.51 60.99 58.51L60.99 58.51L56.78 58.51L56.78 66.61Q56.51 66.71 55.99 66.82Q55.48 66.93 54.89 66.93L54.89 66.93Q52.57 66.93 52.57 65.04L52.57 65.04L52.57 58.51L36.96 58.51Q36.42 58.23 36.01 57.56Q35.61 56.88 35.61 55.97L35.61 55.97Q35.61 55.48 35.69 55.13Q35.77 54.78 35.88 54.45L35.88 54.45ZM52.62 54.89L52.73 37.88L52.62 37.88L40.63 54.89L52.62 54.89ZM78.43 54.78L72.98 54.78L72.98 66.39Q72.65 66.50 72.11 66.63Q71.57 66.77 70.92 66.77L70.92 66.77Q68.55 66.77 68.55 64.77L68.55 64.77L68.55 36.04Q68.55 35.18 68.98 34.72Q69.41 34.26 70.33 33.99L70.33 33.99Q71.90 33.50 74.14 33.29Q76.38 33.07 78.48 33.07L78.48 33.07Q85.34 33.07 88.50 35.96Q91.66 38.85 91.66 43.76L91.66 43.76Q91.66 48.78 88.42 51.78Q85.18 54.78 78.43 54.78L78.43 54.78ZM72.92 51.11L78.16 51.11Q82.48 51.11 84.86 49.33Q87.23 47.54 87.23 43.82L87.23 43.82Q87.23 40.14 84.94 38.44Q82.64 36.74 78.38 36.74L78.38 36.74Q76.92 36.74 75.49 36.88Q74.06 37.01 72.92 37.23L72.92 37.23L72.92 51.11Z%22></path></svg>" />
+    <meta name="description" content="Contents for amateur photographers">
+    <meta name="keywords" content="Photo, presets, courses, repost, galleries">
+    <meta name="author" content="Pierpaolo Di Dato">
+
+    <title>Home</title>
+    <link rel="stylesheet" href="assets/tether/tether.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="assets/dropdown/css/style.css">
+    <link rel="stylesheet" href="assets/socicon/css/styles.css">
+    <link rel="stylesheet" href="assets/theme/css/style.css">
+    <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css">
+    <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+    <link rel="stylesheet" href="style.css" type = "text/css" />
+
+</head>
+
+<body>
+
+    <section class="menu menu3 cid-spaizPbfoR" once="menu" id="menu3-0">
+
+        <nav class="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
+            <div class="container">
+                <div class="navbar-brand">
+                    <span class="navbar-logo">
+                        <a href="index.html">
+                            <img src="assets/images/logo.png" alt="Mobirise" style="height: 3rem;">
+                        </a>
+                    </span>
+                    <span class="navbar-caption-wrap"><a class="navbar-caption text-black display-7"
+                            href="index.html">passion4pics</a></span>
+                </div>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <div class="hamburger">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true">
+                        <li class="nav-item"><a class="nav-link link text-black display-4" href="#">
+                                About us</a></li>
+                        <li class="nav-item"><a class="nav-link link text-black display-4"
+                                href="presets.html">Presets</a></li>
+                        <li class="nav-item"><a class="nav-link link text-black display-4"
+                                href="filters.html" target="_blank">Filters</a>
+                        </li>
+                    </ul>
+                    <div class="icons-menu">
+                        <a class="iconfont-wrapper"
+                            href="https://www.facebook.com/_passion4pics-110868584418609/about/?ref=page_internal"
+                            target="_blank">
+                            <span class="p-2 mbr-iconfont socicon-facebook socicon"></span>
+                        </a>
+                        <a class="iconfont-wrapper" href="https://www.instagram.com/_passion4pics/" target="_blank">
+                            <span class="p-2 mbr-iconfont socicon-instagram socicon"></span>
+                        </a>
+
+                    </div>
+
+                </div>
+            </div>
+        </nav>
+    </section>
+
+    <section class="testimonials1 cid-spaiTlABRY" id="testimonials1-3">
+
+        <div class="container">
+
+            <div class="row align-items-center">
+                <div class="col-12 col-md-6">
+                    <div class="image-wrapper">
+                        <img src="assets/images/photography-801891-1920-1076x717.jpg" alt="Mobirise">
+                    </div>
+                </div>
+                <div class="col-12 col-md">
+                    <div class="text-wrapper">
+                        <p class="mbr-text mbr-fonts-style mb-4 display-7">“Photography is a way of feeling, of
+                            touching, of loving. What you have caught on film is captured forever… It remembers little
+                            things, long after you have forgotten everything.”
+                            <br>
+                        </p>
+                        <p class="name mbr-fonts-style mb-1 display-4"><strong>Aaron Siskind</strong></p>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="content7 cid-ssp6j0FcSc" id="content7-b">
+
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-10">
+                    <blockquote>
+                        <h5 class="mbr-section-title mbr-fonts-style mb-2 display-7"><strong>Our mission</strong></h5>
+                        <p class="mbr-text mbr-fonts-style display-4">Your passion makes you a promising amateur but
+                            your ability to earn a living from your photography work makes you a professional
+                            photographer. Get yourself involved in the community and find all you need to become a
+                            professional.</p>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="gallery1 cid-srZMqP1bME" id="gallery1-a">
+
+
+        <div class="container">
+            <div class="mbr-section-head">
+                <h4 class="mbr-section-title mbr-fonts-style align-center mb-0 display-2">
+                    <strong>Gallery</strong>
+                </h4>
+
+            </div>
+            <div class="row mt-4">
+
+                <div class="item features-image сol-12 col-md-6 col-lg-6">
+                    <div class="item-wrapper">
+                        <div class="item-img">
+                            <img src="assets/images/dsc-0475-2-1108x739.jpg" alt="" title="" data-slide-to="3">
+                        </div>
+                    </div>
+                </div>
+                <div class="item features-image сol-12 col-md-6 col-lg-6">
+                    <div class="item-wrapper">
+                        <div class="item-img">
+                            <img src="assets/images/whatsapp-image-2021-03-18-at-15.23.47-1080x720.jpg" alt=""
+                                data-slide-to="0">
+                        </div>
+                    </div>
+                </div>
+                <div class="item features-image сol-12 col-md-6 col-lg-6">
+                    <div class="item-wrapper">
+                        <div class="item-img">
+                            <img src="assets/images/dsc-303-2048x1366.jpg" alt="" data-slide-to="0">
+                        </div>
+                    </div>
+                </div>
+                <div class="item features-image сol-12 col-md-6 col-lg-6">
+                    <div class="item-wrapper">
+                        <div class="item-img">
+                            <img src="assets/images/dsc-232-2048x1366.jpg" alt="" data-slide-to="0">
+                        </div>
+
+
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </section>
+
+    <section class="contacts4 cid-spajBnt0w5" id="contacts4-5">
+        <div class="container">
+            <div class="row align-items-center justify-content-center">
+                <div class="text-content col-12 col-md-6">
+                    <h2 class="mbr-section-title mbr-fonts-style display-2">
+                        <strong>Follow Us</strong>
+                    </h2>
+                    <p class="mbr-text mbr-fonts-style display-7">Follow us on different channels and in particular tag
+                        us in Instagram with our tag #passion_4_pics. Every week we repost the best pics in our gallery.
+                    </p>
+                </div>
+                <div class="icons d-flex align-items-center col-12 col-md-6 justify-content-end mt-md-0 mt-2 flex-wrap">
+                    <a href="https://www.facebook.com/_passion4pics-110868584418609/about/?ref=page_internal"
+                        target="_blank">
+                        <span class="socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
+                    </a>
+                    <a href="#" target="_blank">
+                        <span class="socicon-youtube socicon mbr-iconfont mbr-iconfont-social"></span>
+                    </a>
+                    <a href="https://www.instagram.com/_passion4pics/" target="_blank">
+                        <span class="socicon-instagram socicon mbr-iconfont mbr-iconfont-social"></span>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="footer7 cid-spaiEcN4DU" once="footers" id="footer7-1">
+        <div class="container">
+            <div class="media-container-row align-center mbr-white">
+                <div class="col-12">
+                    <p class="mbr-text mb-0 mbr-fonts-style display-7">
+                        © Copyright 2021 passion4pics - All Rights Reserved<br>
+                        <a href="mailto:info@passion4pics.net">Email: info@passion4pics.net</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section
+        style="display:none !important;background-color: #fff; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Helvetica Neue', Arial, sans-serif; color:#aaa; font-size:12px; padding: 0; align-items: center; display: flex;">
+        <a href="https://mobirise.site/i" style="flex: 1 1; height: 3rem; padding-left: 1rem;"></a>
+        <p style="flex: 0 0 auto; margin:0; padding-right:1rem;">Web page was <a href="https://mobirise.site/h"
+                style="color:#aaa;">started with</a> Mobirise</p>
+    </section>
+    <script src="assets/web/assets/jquery/jquery.min.js"></script>
+    <script src="assets/popper/popper.min.js"></script>
+    <script src="assets/tether/tether.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/smoothscroll/smooth-scroll.js"></script>
+    <script src="assets/dropdown/js/nav-dropdown.js"></script>
+    <script src="assets/dropdown/js/navbar-dropdown.js"></script>
+    <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+    <script src="assets/sociallikes/social-likes.js"></script>
+    <script src="assets/theme/js/script.js"></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-FT0DP0DEGJ"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'G-FT0DP0DEGJ');
+    </script>
+
+
+</body>
+
+</html>
