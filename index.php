448b3a6f22faa979420ae01c04fb1d7f351e0c33
pierpaolo1989 <pierpaolo.pdd@gmail.com>
2023-11-12 17:24:12 +0100

    General refactor

--- index.php
+++ index.php
@@ -23,63 +23,14 @@
     <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css">
     <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
     <link rel="stylesheet" href="style.css" type = "text/css" />
-
 </head>
 
 <body>
-    <section class="menu menu3 cid-spaizPbfoR" once="menu" id="menu3-0">
-        <nav class="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
-            <div class="container">
-                <div class="navbar-brand">
-                    <span class="navbar-logo">
-                        <a href="index.html">
-                            <img src="assets/images/logo.png" alt="Mobirise" style="height: 3rem;">
-                        </a>
-                    </span>
-                    <span class="navbar-caption-wrap"><a class="navbar-caption text-black display-7"
-                            href="index.html">passion4pics</a></span>
-                </div>
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <div class="hamburger">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true">
-                        <li class="nav-item"><a class="nav-link link text-black display-4" href="#">
-                                About us</a></li>
-                        <li class="nav-item"><a class="nav-link link text-black display-4"
-                                href="presets.html">Presets</a></li>
-                        <li class="nav-item"><a class="nav-link link text-black display-4"
-                                href="filters.html" target="_blank">Filters</a>
-                        </li>
-                    </ul>
-                    <div class="icons-menu">
-                        <a class="iconfont-wrapper"
-                            href="https://www.facebook.com/_passion4pics-110868584418609/about/?ref=page_internal"
-                            target="_blank">
-                            <span class="p-2 mbr-iconfont socicon-facebook socicon"></span>
-                        </a>
-                        <a class="iconfont-wrapper" href="https://www.instagram.com/_passion4pics/" target="_blank">
-                            <span class="p-2 mbr-iconfont socicon-instagram socicon"></span>
-                        </a>
-
-                    </div>
-
-                </div>
-            </div>
-        </nav>
-    </section>
-
+    <!-- HEADER -->
+    <?php include 'views/layout/header.php' ?>
+    
     <section class="testimonials1 cid-spaiTlABRY" id="testimonials1-3">
-
         <div class="container">
-
             <div class="row align-items-center">
                 <div class="col-12 col-md-6">
                     <div class="image-wrapper">
@@ -195,7 +146,7 @@
 
     </section>
     <!-- FOOTER -->
-    <?php include 'footer.php' ?>
+    <?php include 'views/layout/footer.php' ?>
     <section
         style="display:none !important;background-color: #fff; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Helvetica Neue', Arial, sans-serif; color:#aaa; font-size:12px; padding: 0; align-items: center; display: flex;">
         <a href="https://mobirise.site/i" style="flex: 1 1; height: 3rem; padding-left: 1rem;"></a>
